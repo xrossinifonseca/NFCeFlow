75efa9b75266dca2a3c13dfacb3bec0bc9a31b94
Rossini fonseca <rossinidrum@outlook.com>
2024-08-14 18:29:46 -0300

    test: Add  test for Product model
    - Confirm that a valid product can be built with correct attributes.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is a valid product" do
+    product = build(:product)
+    expect(product).to be_valid
+  end
+
+
 end
