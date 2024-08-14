0d456aa1c695905f86b3d9f36fae802ceba0f6d2
Rossini fonseca <rossinidrum@outlook.com>
2024-08-14 17:58:00 -0300

    test: add issuer factory

---
+++ spec/factories/issuer.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :issuer do
+    cnpj { '12345678000195' }
+    nome { 'Empresa Exemplo LTDA' }
+    nome_fantasia {'Exemplo'}
+    logradouro {"Rua Exemplo"}
+    numero {'123'}
+    municipio {'Cidade Exemplo'}
+    codigo_municipio {1234567}
+    bairro {'Bairro Exemplo'}
+    pais {'Brasil'}
+    uf {'SP'}
+    cep {'12345678'}
+end
+end
