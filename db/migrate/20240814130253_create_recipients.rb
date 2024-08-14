34e8ea8996e15d2f14e99e251097ecec798fe05d
Rossini fonseca <rossinidrum@outlook.com>
2024-08-14 18:04:07 -0300

    feat: add CreateRecipients migrations

---
+++ db/migrate/20240814130253_create_recipients.rb
@@ -0,0 +1,22 @@
+class CreateRecipients < ActiveRecord::Migration[7.1]
+  def change
+    create_table :recipients do |t|
+      t.string :cnpj, limit: 14, null: false
+      t.string :nome, null: false
+      t.string :logradouro
+      t.string :numero, limit: 10
+      t.string :municipio
+      t.integer :codigo_municipio
+      t.string :bairro
+      t.string :pais
+      t.string :uf, limit: 2
+      t.string :cep, limit: 8
+      t.string :telefone
+
+      t.timestamps
+    end
+
+    add_index :recipients, :cnpj, unique: true
+
+  end
+end
