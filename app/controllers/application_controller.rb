cae936bb722b281ab552db95528e72ba728ed3d2
Rossini fonseca <rossinidrum@outlook.com>
2024-08-14 09:35:05 -0300

    feat: add before_action :authenticate_customer!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_customer!
+
+
 
   protected
 
