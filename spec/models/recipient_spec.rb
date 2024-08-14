8692571ebbb784f43cb8c2e54069af1ad7a97b34
Rossini fonseca <rossinidrum@outlook.com>
2024-08-14 18:21:16 -0300

    test: Add validation tests for recipient model
    - Ensure that an recipient is invalid if the CNPJ is already taken.
    - Ensure that a recipient is invalid if the CNPJ is not present.
    - Confirm that a valid recipient can be built with correct attributes.

--- spec/models/recipient_spec.rb
+++ spec/models/recipient_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Recipient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'is a valid recipient' do
+    recipient = build(:recipient)
+    expect(recipient).to be_valid
+  end
+
+
+  describe "validations" do
+
+    it "is not valid if cnpj has already taken" do
+
+      recipient = Recipient.create(
+               cnpj: '12345678000195',
+               nome: 'Empresa Exemplo LTDA',
+               logradouro: 'Rua Exemplo',
+               numero: '123',
+               municipio: 'Cidade Exemplo',
+                codigo_municipio: 1234567,
+                bairro: 'Bairro Exemplo',
+                pais: 'Brasil',
+                uf: 'SP',
+                cep: '12345678',
+                 telefone: '1122334455',
+      )
+
+      recipient_2 = build(:recipient,cnpj:"12345678000195")
+
+
+      expect(recipient_2).not_to be_valid
+      expect(recipient_2.errors[:cnpj]).to include("has already been taken")
+
+    end
+
+
+    it "is not valid if cnpj is not present" do
+      recipient_2 = build(:recipient,cnpj:nil)
+      expect(recipient_2).not_to be_valid
+      expect(recipient_2.errors[:cnpj]).to include("can't be blank")
+    end
+
+    end
+
+
+
+
+
 end
