442b01a4bb1f493b3758223b77bef486805b6f3b
Rossini fonseca <rossinidrum@outlook.com>
2024-08-15 10:03:52 -0300

    test: add validation for product uniqueness by name, NCM, and CFOP

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,5 +7,16 @@ RSpec.describe Product, type: :model do
     expect(product).to be_valid
   end
 
+  it "is not valid if the product nome, ncm and cfop has already been taken" do
+
+    product = Product.create!(nome:"test",ncm:"12345678",cfop:"5101")
+    product_2 = build(:product,nome:"test",ncm:"12345678",cfop:"5101")
+
+    expect(product_2).not_to be_valid
+    expect(product_2.errors[:nome]).to include("has already been taken")
+
+  end
+
+
 
 end
