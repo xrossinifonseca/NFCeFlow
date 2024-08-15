2e42e2f09182a552a8e9edcf09c920d25248e48b
Rossini fonseca <rossinidrum@outlook.com>
2024-08-15 09:42:49 -0300

    add  dependent: :destroy for tax

--- app/models/nfce.rb
+++ app/models/nfce.rb
@@ -2,7 +2,7 @@ class Nfce < ApplicationRecord
   belongs_to :customer
   belongs_to :issuer
   belongs_to :recipient
-  has_one :tax
+  has_one :tax, dependent: :destroy
   has_many :nfce_products, dependent: :destroy
   has_many :products, through: :nfce_products
 
