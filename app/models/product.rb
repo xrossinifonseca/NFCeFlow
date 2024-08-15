00788358109d557a669b966eea5f7a5e0893ad6b
Rossini fonseca <rossinidrum@outlook.com>
2024-08-15 10:03:19 -0300

    feat: ensure uniqueness of product by name, NCM, and CFOP combination

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   has_many :nfce_products, dependent: :destroy
   has_many :nfces, through: :nfce_products
 
+  validates :nome, uniqueness: {scope: [:ncm, :cfop]}
+
 end
