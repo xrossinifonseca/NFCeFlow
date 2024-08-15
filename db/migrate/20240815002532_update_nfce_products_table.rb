7463e5853c93deedfe0865ab8dfbbc0dd8c4c9a1
Rossini fonseca <rossinidrum@outlook.com>
2024-08-14 21:46:40 -0300

    Update NfceProduct table schema

---
+++ db/migrate/20240815002532_update_nfce_products_table.rb
@@ -0,0 +1,18 @@
+class UpdateNfceProductsTable < ActiveRecord::Migration[7.1]
+  def change
+    change_table :nfce_products do |t|
+      t.decimal :valor_total, precision: 10, scale: 2
+
+      t.remove :quantidade
+
+      t.change :unidade_comercializada, :string
+      t.change :quantidade_comercializada, :decimal, precision: 10, scale: 2
+      t.change :valor_unitario, :decimal, precision: 10, scale: 2
+      t.change :valor_icms, :decimal, precision: 10, scale: 2
+      t.change :valor_ipi, :decimal, precision: 10, scale: 2
+
+      t.change :product_id, :integer
+      t.change :nfce_id, :integer
+
+  end
+end
