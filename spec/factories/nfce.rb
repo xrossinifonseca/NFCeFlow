7a8adac7ee1fe23b714e243757989dbe93a9ef2d
Rossini fonseca <rossinidrum@outlook.com>
2024-08-15 09:42:13 -0300

    update nfce factory

--- spec/factories/nfce.rb
+++ spec/factories/nfce.rb
@@ -4,6 +4,9 @@ FactoryBot.define do
   serie {'A'}
   numero_nota {'0001'}
   data_emissao {'2024-08-14'}
+  valor_total_desconto {0.0}
+  valor_total_produtos {2500}
+  valor_total {2700}
   association :customer
   association :recipient
   association :issuer
