f18fa70e12fe940cc51a481745614c1543a49a0b
Rossini fonseca <rossinidrum@outlook.com>
2024-08-17 17:44:20 -0300

    test: unsure that is a valid tax

--- spec/models/tax_spec.rb
+++ spec/models/tax_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Tax, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is a valid tax" do
+    product = build(:tax)
+    expect(product).to be_valid
+  end
 end
