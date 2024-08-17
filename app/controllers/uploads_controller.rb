059a023a3a83a34299697b5d41d3b6ac10102ba0
Rossini fonseca <rossinidrum@outlook.com>
2024-08-17 15:53:46 -0300

    removes structure from local file storage to in-memory storage

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -16,38 +16,19 @@ def create
     return redirect_to new_upload_path
   end
 
-  file_path = Rails.root.join('tmp', 'uploads', file.original_filename)
 
-  File.open(file_path, 'wb') do |f|
-    f.write(file.read)
-  end
+    mime_type = MimeMagic.by_path(file.path)
 
-  mime_type = MimeMagic.by_path(file_path)
+    if mime_type.subtype == "xml"
 
-   if File.exist?(file_path)
+      proccess_xml_file(file)
 
-    if mime_type.subtype == "zip"
-      ZipFileProcessingService.process(file_path,current_customer.id)
-      flash[:success] = "your zip file is being processed"
+    elsif mime_type.subtype == "zip"
 
-      redirect_to uploads_path
+      proccess_zip_file(file)
 
-      elsif mime_type.subtype == "xml"
-
-        upload = Upload.create!(file_name:file.original_filename, status:"processing", customer_id:current_customer.id)
-
-        XmlProcessingWorkerJob.perform_in(60,file_path.to_s,current_customer.id,upload.id)
-
-        flash[:success] = "your xml file is being processed"
-
-        redirect_to uploads_path
-
-      else
-        flash[:error] = "The file must be in XML or ZIP format. Please upload a file with one of the supported extensions."
-        return redirect_to new_upload_path
-      end
     else
-      flash[:error] = "Failed to save the file. Please try again."
+      flash[:error] = "The file must be in XML or ZIP format. Please upload a file with one of the supported extensions."
       return redirect_to new_upload_path
     end
 end
@@ -55,5 +36,22 @@ end
 
 
 
+def proccess_xml_file(file)
 
+      file_content = file.read
+      file_temp = StringIO.new(file_content)
+
+      upload = Upload.create!(file_name:file.original_filename, status:"processing", customer_id:current_customer.id)
+      XmlProcessingWorkerJob.perform_async(file_content ,current_customer.id,upload.id)
+      flash[:success] = "your xml file is being processed"
+      redirect_to uploads_path
+end
+
+
+
+def proccess_zip_file(file)
+      ZipFileProcessingService.process(file,current_customer.id)
+      flash[:success] = "your zip file is being processed"
+      redirect_to uploads_path
+end
 end
