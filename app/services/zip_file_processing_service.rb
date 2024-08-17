742afc8369d0fd3eb365923ca4a64c756b25cc9c
Rossini fonseca <rossinidrum@outlook.com>
2024-08-17 15:54:06 -0300

    removes structure from local file storage to in-memory storage

--- app/services/zip_file_processing_service.rb
+++ app/services/zip_file_processing_service.rb
@@ -9,34 +9,27 @@ class ZipFileProcessingService
 
         if entry.file? && File.extname(entry.name) == ".xml"
 
-          file_name =  entry.name.gsub(" ", "_")
-
-          extracted_file_path = Rails.root.join('tmp', 'uploads', file_name)
-
-
-          FileUtils.mkdir_p(File.dirname(extracted_file_path))
-
           begin
-            entry.extract(extracted_file_path) { true }
+            entry.get_input_stream do |io|
+              file_content = io.read
+              upload = Upload.create!(file_name: entry.name, status: "processing", customer_id: customer_id)
+              XmlProcessingWorkerJob.perform_async(file_content, customer_id, upload.id)
+            end
 
           rescue => e
-            Upload.create!(file_name: file_name, status: "failed", customer_id: customer_id,message:"failed to extract file")
-            Rails.logger.error "Erro ao extrair #{entry.name}: #{e.message}"
-            next
+            Upload.create!(file_name: entry.name, status: "failed", customer_id: customer_id, error_message: "Failed to process file: #{e.message}")
+            Rails.logger.error "Failed to process #{entry.name}: #{e.message}"
           end
 
-          upload = Upload.create!(file_name: file_name, status: "processing", customer_id: customer_id)
-          XmlProcessingWorkerJob.perform_async(extracted_file_path.to_s, customer_id, upload.id)
         end
+
       end
 
-      File.delete(file) if File.exist?(file)
+
     end
+
+
+
+
   end
 end
-
-
-# elsif entry.directory?
-#   puts "#{entry.name} is a directory!"
-# elsif entry.symlink?
-#   puts "#{entry.name} is a symlink!"
