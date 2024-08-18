a262937eaef7968533a21077fa4b2dc086641f29
Rossini fonseca <rossinidrum@outlook.com>
2024-08-18 19:35:07 -0300

    includes logs

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -39,11 +39,17 @@ end
 def proccess_xml_file(file)
 
       file_content = file.read
+
       file_temp = StringIO.new(file_content)
 
       upload = Upload.create!(file_name:file.original_filename, status:"processing", customer_id:current_customer.id)
+
       XmlProcessingWorkerJob.perform_async(file_content ,current_customer.id,upload.id)
+
       flash[:success] = "your xml file is being processed"
+
+      Rails.logger.error "Customer #{current_customer.name} user processed a xml file on the system"
+
       redirect_to uploads_path
 end
 
@@ -51,7 +57,11 @@ end
 
 def proccess_zip_file(file)
       ZipFileProcessingService.process(file,current_customer.id)
+
       flash[:success] = "your zip file is being processed"
+
+      Rails.logger.error "Customer #{current_customer.name} user processed a zip file on the system"
+
       redirect_to uploads_path
 end
 end
